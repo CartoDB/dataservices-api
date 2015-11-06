97af24c2e08b1f17c6ec85092a7bdcac68d97aca
Guido Fioravantti <guido.fioravantti@gmail.com>
2015-11-06 18:28:29 +0100

    Removes unused functions

--- server/extension/cdb_geocoder_server--0.0.1.sql
+++ server/extension/cdb_geocoder_server--0.0.1.sql
@@ -1,25 +1,6 @@
 -- Complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION cdb_geocoder_server" to load this file. \quit
 -- Check if a given host is up by performing a ping -c 1 call.
-CREATE OR REPLACE FUNCTION cdb_geocoder_server._check_host(hostname TEXT)
-  RETURNS BOOLEAN
-AS $$
-  import os
-
-  response = os.system("ping -c 1 " + hostname)
-
-  return False if response else True
-$$ LANGUAGE plpythonu VOLATILE;
-
--- Returns current pwd
-CREATE OR REPLACE FUNCTION cdb_geocoder_server._pwd()
-  RETURNS TEXT
-AS $$
-  import os
-
-  return os.getcwd()
-$$ LANGUAGE plpythonu VOLATILE;
--- Check if a given host is up by performing a ping -c 1 call.
 CREATE OR REPLACE FUNCTION cdb_geocoder_server.geocode_street(searchtext TEXT, state TEXT DEFAULT NULL, country TEXT DEFAULT NULL)
   RETURNS Geometry
 AS $$
--- server/extension/sql/10_helper_test.sql
+++
@@ -1,5 +0,0 @@
--- Check that check_host is working
-select cdb_geocoder_server._check_host('cartodb.com');
-
--- Check that check_pwd is working
-select cdb_geocoder_server._pwd();
