4aa757ac88292d06ae3e81318118442bd4a88537
Carla Iriberri <iriberrig@gmail.com>
2015-11-11 18:14:22 +0100

    Adds namedplace function with 3 params #6

--- server/extension/sql/0.0.1/50_namedplaces.sql
+++ server/extension/sql/0.0.1/50_namedplaces.sql
@@ -44,6 +44,28 @@ RETURNS Geometry AS $$
     return rv[0]["mypoint"]
 $$ LANGUAGE plpythonu;
 
+---- geocode_namedplace(city_name text, admin1_name text, country_name text)
+CREATE OR REPLACE FUNCTION geocode_namedplace(user_id name, tx_id bigint, city_name text, admin1_name text, country_name text)
+RETURNS Geometry AS $$
+    plpy.debug('Entering geocode_namedplace(city_name text, admin1_name text, country_name text)')
+    plpy.debug('user_id = %s' % user_id)
+
+    #-- Access control
+    #-- TODO: this should be part of cdb python library
+    if user_id == 'publicuser':
+        plpy.error('The api_key must be provided')
+
+    #--TODO: rate limiting check
+    #--TODO: quota check
+
+    #-- Copied from the doc, see http://www.postgresql.org/docs/9.4/static/plpython-database.html
+    plan = plpy.prepare("SELECT cdb_geocoder_server._geocode_namedplace($1, $2, $3) AS mypoint", ["text", "text", "text"])
+    rv = plpy.execute(plan, [city_name, admin1_name, country_name], 1)
+
+    plpy.debug('Returning from Returning from geocode_namedplace')
+    return rv[0]["mypoint"]
+$$ LANGUAGE plpythonu;
+
 --------------------------------------------------------------------------------
 
 -- Implementation of the server extension
@@ -82,6 +104,30 @@ RETURNS Geometry AS $$
         SELECT geom FROM best WHERE geom IS NOT NULL
         UNION ALL
         SELECT geom FROM next
+   ) v;
+
+    RETURN ret;
+  END
+$$ LANGUAGE plpgsql;
+
+---- geocode_namedplace(city_name text, admin1_name text, country_name text)
+CREATE OR REPLACE FUNCTION _geocode_namedplace(city_name text, admin1_name text, country_name text)
+RETURNS Geometry AS $$
+  DECLARE
+    ret Geometry;
+  BEGIN
+  SELECT geom INTO ret
+  FROM (
+    WITH inputcountry AS (
+        SELECT iso2 as isoTwo FROM country_decoder WHERE lower(country_name) = ANY (synonyms) LIMIT 1
+        ),
+    p AS (
+       SELECT r.s, r.a1, (SELECT admin1 FROM admin1_decoder, inputcountry WHERE lower(r.a1) = ANY (synonyms) AND admin1_decoder.iso2 = inputcountry.isoTwo LIMIT 1) i FROM (SELECT city_name AS s, admin1_name::text AS a1) r),
+       best AS (SELECT p.s AS q, p.a1 as a1, (SELECT gp.the_geom AS geom FROM global_cities_points_limited gp WHERE gp.lowername = lower(p.s) AND gp.admin1 = p.i ORDER BY population DESC LIMIT 1) AS geom FROM p),
+       next AS (SELECT p.s AS q, p.a1 AS a1, (SELECT gp.the_geom FROM global_cities_points_limited gp, global_cities_alternates_limited ga WHERE lower(p.s) = ga.lowername AND ga.admin1 = p.i AND ga.geoname_id = gp.geoname_id ORDER BY preferred DESC LIMIT 1) geom FROM p WHERE p.s NOT IN (SELECT q FROM best WHERE geom IS NOT NULL))
+       SELECT geom FROM best WHERE geom IS NOT NULL
+       UNION ALL
+       SELECT geom FROM next
    ) v;
 
     RETURN ret;
