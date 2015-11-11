b154dadc84b68c5526f19385ffb07f9ede8319a0
Rafa de la Torre <rtorre@cartodb.com>
2015-11-11 18:52:48 +0100

    Add a test that makes a bit of sense

--- client/sql/00_installation_test.sql
+++ client/sql/00_installation_test.sql
@@ -4,3 +4,9 @@ CREATE EXTENSION plproxy;
 
 -- Install the extension
 CREATE EXTENSION cdb_geocoder_client;
+
+-- Mock the server connection to point to this very test db
+SELECT cdb_geocoder_client._config_set('db_server_config', '{"connection_str": "dbname=contrib_regression host=127.0.0.1 user=postgres"}');
+
+-- Mock the server schema
+CREATE SCHEMA cdb_geocoder_server;
