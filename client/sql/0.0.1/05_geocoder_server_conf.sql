65ffb5d6454725d84f960c238579a450449da270
Mario de Frutos <mario.defrutos@cartodb.com>
2015-11-11 16:52:45 +0100

    Added schema to client functions and fixed some errors

--- client/sql/0.0.1/05_geocoder_server_conf.sql
+++ client/sql/0.0.1/05_geocoder_server_conf.sql
@@ -4,35 +4,35 @@
 -- The table and the function are considered to be private and therefore
 -- no permissions are granted for any other user but the creator.
 
-CREATE TABLE IF NOT EXISTS _config ( KEY TEXT PRIMARY KEY, VALUE JSON NOT NULL );
+CREATE TABLE IF NOT EXISTS cdb_geocoder_client._config ( KEY TEXT PRIMARY KEY, VALUE JSON NOT NULL );
 
 -- Needed to dump config in backups
 -- This can only be called from an SQL script executed by CREATE EXTENSION
-SELECT pg_catalog.pg_extension_config_dump('_config', '');
+SELECT pg_catalog.pg_extension_config_dump('cdb_geocoder_client._config', '');
 
 
-CREATE OR REPLACE FUNCTION _config_set(key text, value JSON)
+CREATE OR REPLACE FUNCTION cdb_geocoder_client._config_set(key text, value JSON)
 RETURNS VOID AS $$
 BEGIN
-    PERFORM _config_remove(key);
-    EXECUTE 'INSERT INTO _config (KEY, VALUE) VALUES ($1, $2);' USING key, value;
+    PERFORM cdb_geocoder_client._config_remove(key);
+    EXECUTE 'INSERT INTO cdb_geocoder_client._config (KEY, VALUE) VALUES ($1, $2);' USING key, value;
 END
 $$ LANGUAGE PLPGSQL VOLATILE;
 
 
-CREATE OR REPLACE FUNCTION _config_remove(key text)
+CREATE OR REPLACE FUNCTION cdb_geocoder_client._config_remove(key text)
 RETURNS VOID AS $$
 BEGIN
-    EXECUTE 'DELETE FROM _config WHERE KEY = $1;' USING key;
+    EXECUTE 'DELETE FROM cdb_geocoder_client._config WHERE KEY = $1;' USING key;
 END
 $$ LANGUAGE PLPGSQL VOLATILE;
 
-CREATE OR REPLACE FUNCTION _config_get(key text)
+CREATE OR REPLACE FUNCTION cdb_geocoder_client._config_get(key text)
     RETURNS JSON AS $$
 DECLARE
     value JSON;
 BEGIN
-    EXECUTE 'SELECT VALUE FROM _config WHERE KEY = $1;' INTO value USING key;
+    EXECUTE 'SELECT VALUE FROM cdb_geocoder_client._config WHERE KEY = $1;' INTO value USING key;
     RETURN value;
 END
 $$ LANGUAGE PLPGSQL STABLE;
