66fd33da374d12bebddc89a11db0b3d307b7f7fd
Guido Fioravantti <guido.fioravantti@gmail.com>
2015-11-11 15:43:11 +0100

    Fixes param type for geocode_ip

--- server/extension/sql/0.0.1/90_geocode_ip.sql
+++ server/extension/sql/0.0.1/90_geocode_ip.sql
@@ -1,6 +1,6 @@
 -- Interface of the server extension
 
-CREATE OR REPLACE FUNCTION geocode_ip_point(user_id NAME, tx_id BIGINT, ip INET)
+CREATE OR REPLACE FUNCTION geocode_ip_point(user_id NAME, tx_id BIGINT, ip TEXT)
 RETURNS Geometry AS $$
     plpy.debug('Entering _geocode_ip_point')
     plpy.debug('user_id = %s' % user_id)
@@ -14,7 +14,7 @@ RETURNS Geometry AS $$
     #--TODO: quota check
 
     #-- Copied from the doc, see http://www.postgresql.org/docs/9.4/static/plpython-database.html
-    plan = plpy.prepare("SELECT cdb_geocoder_server._geocode_ip_point($1) AS point", ["INET"])
+    plan = plpy.prepare("SELECT cdb_geocoder_server._geocode_ip_point($1) AS point", ["TEXT"])
     rv = plpy.execute(plan, [ip], 1)
 
     plpy.debug('Returning from _geocode_ip_point')
@@ -26,14 +26,14 @@ $$ LANGUAGE plpythonu;
 
 -- Implementation of the server extension
 -- Note: these functions depend on the cdb_geocoder extension
-CREATE OR REPLACE FUNCTION _geocode_ip_point(ip INET)
+CREATE OR REPLACE FUNCTION _geocode_ip_point(ip TEXT)
 RETURNS Geometry AS $$
   DECLARE
     ret Geometry;
   BEGIN
     SELECT ips.the_geom as geom INTO ret
       FROM public.ip_address_locations ips
-        WHERE ips.network_start_ip = ip;
+        WHERE ips.network_start_ip = ip::inet;
 
     RETURN ret;
   END
