1c44fbbf56ca1165093c89f338745ad7fe4b51c9
Guido Fioravantti <guido.fioravantti@gmail.com>
2015-11-11 15:38:18 +0100

    Adds 90_geocode_ip and tests

---
+++ server/extension/sql/0.0.1/90_geocode_ip.sql
@@ -0,0 +1,40 @@
+-- Interface of the server extension
+
+CREATE OR REPLACE FUNCTION geocode_ip_point(user_id NAME, tx_id BIGINT, ip INET)
+RETURNS Geometry AS $$
+    plpy.debug('Entering _geocode_ip_point')
+    plpy.debug('user_id = %s' % user_id)
+
+    #-- Access control
+    #-- TODO: this should be part of cdb python library
+    if user_id == 'publicuser':
+        plpy.error('The api_key must be provided')
+
+    #--TODO: rate limiting check
+    #--TODO: quota check
+
+    #-- Copied from the doc, see http://www.postgresql.org/docs/9.4/static/plpython-database.html
+    plan = plpy.prepare("SELECT cdb_geocoder_server._geocode_ip_point($1) AS point", ["INET"])
+    rv = plpy.execute(plan, [ip], 1)
+
+    plpy.debug('Returning from _geocode_ip_point')
+    return rv[0]["point"]
+$$ LANGUAGE plpythonu;
+
+
+--------------------------------------------------------------------------------
+
+-- Implementation of the server extension
+-- Note: these functions depend on the cdb_geocoder extension
+CREATE OR REPLACE FUNCTION _geocode_ip_point(ip INET)
+RETURNS Geometry AS $$
+  DECLARE
+    ret Geometry;
+  BEGIN
+    SELECT ips.the_geom as geom INTO ret
+      FROM public.ip_address_locations ips
+        WHERE ips.network_start_ip = ip;
+
+    RETURN ret;
+  END
+$$ LANGUAGE plpgsql;
---
+++ server/extension/sql/90_geocode_ip_test.sql
@@ -0,0 +1,9 @@
+-- Check that the public function is callable, even with no data
+-- It should return NULL
+SELECT cdb_geocoder_server.geocode_ip_point(session_user, txid_current(), '0.0.0.0'::inet);
+
+-- Insert dummy data into ip_address_locations
+INSERT INTO ip_address_locations VALUES ('0.0.0.0'::inet, (ST_SetSRID(ST_MakePoint('40.40', '3.71'), 4326)));
+
+-- This should return the polygon inserted above
+SELECT cdb_geocoder_server.geocode_ip_point(session_user, txid_current(), '0.0.0.0'::inet);
