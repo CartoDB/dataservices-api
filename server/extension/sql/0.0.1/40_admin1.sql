93513d695494de580869ce3b3ee8bf4464602af5
Carla Iriberri <iriberrig@gmail.com>
2015-11-11 14:05:17 +0100

    Adding a new function

--- server/extension/sql/0.0.1/40_admin1.sql
+++ server/extension/sql/0.0.1/40_admin1.sql
@@ -1,8 +1,9 @@
--- Interface of the server extension
+-- Interfacess of the server extension
 
+---- geocode_admin1_polygons(admin1_name text)
 CREATE OR REPLACE FUNCTION geocode_admin1_polygons(user_id name, tx_id bigint, admin1_name text)
 RETURNS Geometry AS $$
-    plpy.debug('Entering geocode_admin1_polygons')
+    plpy.debug('Entering geocode_admin1_polygons(admin1_name text)')
     plpy.debug('user_id = %s' % user_id)
 
     #-- Access control
@@ -21,11 +22,34 @@ RETURNS Geometry AS $$
     return rv[0]["mypolygon"]
 $$ LANGUAGE plpythonu;
 
+---- geocode_admin1_polygons(admin1_name text, country_name text)
+CREATE OR REPLACE FUNCTION geocode_admin1_polygons(user_id name, tx_id bigint, admin1_name text, country_name text)
+RETURNS Geometry AS $$
+    plpy.debug('Entering geocode_admin1_polygons(admin1_name text, country_name text)')
+    plpy.debug('user_id = %s' % user_id)
+
+    #-- Access control
+    #-- TODO: this should be part of cdb python library
+    if user_id == 'publicuser':
+        plpy.error('The api_key must be provided')
+
+    #--TODO: rate limiting check
+    #--TODO: quota check
+
+    #-- Copied from the doc, see http://www.postgresql.org/docs/9.4/static/plpython-database.html
+    plan = plpy.prepare("SELECT cdb_geocoder_server._geocode_admin1_polygons($1, $2) AS mypolygon", ["text", "text"])
+    rv = plpy.execute(plan, [admin1_name, country_name], 1)
+
+    plpy.debug('Returning from Returning from geocode_admin1_polygons(admin1_name text, country_name text)')
+    return rv[0]["mypolygon"]
+$$ LANGUAGE plpythonu;
 
 --------------------------------------------------------------------------------
 
 -- Implementation of the server extension
 -- Note: these functions depend on the cdb_geocoder extension
+
+---- geocode_admin1_polygons(admin1_name text)
 CREATE OR REPLACE FUNCTION _geocode_admin1_polygons(admin1_name text)
 RETURNS Geometry AS $$
   DECLARE
@@ -48,3 +72,28 @@ RETURNS Geometry AS $$
     RETURN ret;
   END
 $$ LANGUAGE plpgsql;
+
+---- geocode_admin1_polygons(admin1_name text, country_name text)
+CREATE OR REPLACE FUNCTION _geocode_admin1_polygons(admin1_name text, country_name text)
+RETURNS Geometry AS $$
+  DECLARE
+    ret Geometry;
+  BEGIN
+    WITH p AS (SELECT r.c, r.q, (SELECT iso3 FROM country_decoder WHERE lower(country_name) = ANY (synonyms)) i FROM (SELECT  trim(replace(lower(admin1_name),'.',' ')) c, country_name q) r)
+    SELECT
+      geom INTO ret
+    FROM (
+      SELECT 
+        q, (
+          SELECT the_geom 
+          FROM global_province_polygons
+          WHERE p.c = ANY (synonyms) 
+          AND iso3 = p.i
+          ORDER BY frequency DESC LIMIT 1
+        ) geom
+      FROM p) n;
+
+    RETURN ret;
+  END
+$$ LANGUAGE plpgsql;
+
