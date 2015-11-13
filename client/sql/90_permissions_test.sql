50d1f502f7d0d1428c6a4846ccc021233b6b769c
Carla Iriberri <iriberrig@gmail.com>
2015-11-13 12:32:36 +0100

    Adds permissions and their tests

--- client/sql/90_permissions_test.sql
+++ client/sql/90_permissions_test.sql
@@ -4,9 +4,25 @@ SET ROLE test_regular_user;
 -- Exercise the public function
 -- it is public, it shall work
 SELECT cdb_geocoder_client.geocode_admin0_polygons('Spain');
+SELECT cdb_geocoder_client.geocode_admin1_polygon('California');
+SELECT cdb_geocoder_client.geocode_admin1_polygon('California', 'United States');
+SELECT cdb_geocoder_client.geocode_namedplace_point('Elx');
+SELECT cdb_geocoder_client.geocode_namedplace_point('Elx', 'Valencia');
+SELECT cdb_geocoder_client.geocode_namedplace_point('Elx', 'Valencia', 'Spain');
+SELECT cdb_geocoder_client.geocode_postalcode_polygon('03204', 'Spain');
+SELECT cdb_geocoder_client.geocode_postalcode_polygon(3204, 'Spain');
+SELECT cdb_geocoder_client.geocode_ip('8.8.8.8');
 
 -- Check the regular user has no permissions on private functions
 SELECT cdb_geocoder_client._geocode_admin0_polygons('evil_user', 666, 'Hell');
+SELECT cdb_geocoder_client._geocode_admin1_polygon('evil_user', 666, 'Hell');
+SELECT cdb_geocoder_client._geocode_admin1_polygon('evil_user', 666, 'Sheol', 'Hell');
+SELECT cdb_geocoder_client._geocode_namedplace_point('evil_user', 666, 'Sheol');
+SELECT cdb_geocoder_client._geocode_namedplace_point('evil_user', 666, 'Sheol', 'Hell');
+SELECT cdb_geocoder_client._geocode_namedplace_point('evil_user', 666, 'Sheol', 'Hell', 'Ugly world');
+SELECT cdb_geocoder_client._geocode_postalcode_polygon('evil_user', 666, '66666', 'Hell');
+SELECT cdb_geocoder_client._geocode_postalcode_polygon('evil_user', 666, 66666, 'Hell');
+SELECT cdb_geocoder_client._geocode_ip('evil_user', 666, '8.8.8.8');
 
 -- Check the regular user cannot look into config table
 SELECT * from cdb_geocoder_client._config;
