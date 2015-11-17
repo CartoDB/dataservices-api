5d7cfa9053610cbe86b150992612eff75a82a99d
Carla <carla@cartodb.com>
2015-11-17 10:19:48 +0100

    Update 10_admin0_test.sql

--- client/sql/10_admin0_test.sql
+++ client/sql/10_admin0_test.sql
@@ -2,7 +2,7 @@
 CREATE OR REPLACE FUNCTION cdb_geocoder_server.geocode_admin0_polygons(user_id name, tx_id bigint, country_name text)
 RETURNS Geometry AS $$
 BEGIN
-  RAISE NOTICE 'cbd_geocoder_server.geocode_admin0_polygons invoked with params (%, %, %)', user_id, 'some_transaction_id', country_name;
+  RAISE NOTICE 'cdb_geocoder_server.geocode_admin0_polygons invoked with params (%, %, %)', user_id, 'some_transaction_id', country_name;
   RETURN NULL;
 END;
 $$ LANGUAGE 'plpgsql';
