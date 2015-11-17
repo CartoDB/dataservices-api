e0259a8b8c2a5b4544b8823f269d8d7ed6b2ff52
Rafa de la Torre <rtorre@cartodb.com>
2015-11-17 16:28:33 +0100

    Add code generator for public API function definitions and their plproxied counterparts

--- interface/generate-grant-execute.rb
+++ interface/generate-grant-execute.rb
@@ -23,9 +23,28 @@ class GrantExecute
   end  
 end
 
+class PublicFunctionDefinition
+  TEMPLATE_FILE = 'templates/public-function-definition.erb'
+
+  attr_reader :function_signature
+
+  def initialize(function_signature)
+    @function_signature = function_signature
+    @template = File.read(TEMPLATE_FILE)
+  end
+
+  def render
+    ERB.new(@template).result(binding)
+  end
+end
 
 
 CSV.foreach(INTERFACE_SOURCE_FILE, {headers: true}) do |function_signature|
+
+  function_definition = PublicFunctionDefinition.new(function_signature).render
+  puts function_definition
+
+
   grant = GrantExecute.new(function_signature).render
-  puts grant
+  #puts grant
 end
