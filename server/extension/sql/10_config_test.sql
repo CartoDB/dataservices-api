fa1d7af4100bc0d8c1868c1e62644ddd222698a0
Mario de Frutos <mario.defrutos@cartodb.com>
2015-11-23 12:53:46 +0100

    Deleted unused test for config tables

--- server/extension/sql/10_config_test.sql
+++
@@ -1,8 +0,0 @@
--- Create a conf table
-SELECT cartodb.cdb_conf_setconf('cds', '{"Manolo Escobar": {"El Limonero":"En stock", "Viva el vino":"Sin stock"}}');
-
--- Test key retrieval
-SELECT cartodb.cdb_conf_getconf('cds');
-
--- Test returns NULL if key doesn't exist
-SELECT cartodb.cdb_conf_getconf('no existe');
