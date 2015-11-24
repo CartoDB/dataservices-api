2c7223b385d67bdac752659301eb4fffb46cd3af
Mario de Frutos <mario.defrutos@cartodb.com>
2015-11-24 10:06:55 +0100

    Revert "Change ipaddress geocode to match the server signature"
    This reverts commit b57dc82f422ae3cd8b531936dc58ebf3347828e2.

--- client/sql/90_permissions_test.sql
+++ client/sql/90_permissions_test.sql
@@ -11,7 +11,7 @@ SELECT cdb_geocoder_client.geocode_namedplace_point('Elx', 'Valencia');
 SELECT cdb_geocoder_client.geocode_namedplace_point('Elx', 'Valencia', 'Spain');
 SELECT cdb_geocoder_client.geocode_postalcode_polygon('03204', 'Spain');
 SELECT cdb_geocoder_client.geocode_postalcode_point('03204', 'Spain');
-SELECT cdb_geocoder_client.geocode_ip_point('8.8.8.8');
+SELECT cdb_geocoder_client.geocode_ipaddress_point('8.8.8.8');
 
 -- Check the regular user has no permissions on private functions
 SELECT cdb_geocoder_client._geocode_admin0_polygon('evil_user', '{"is_organization": false, "entity_name": "test_user"}', '{"street_geocoder_provider": "nokia","nokia_monthly_quota": 100, "nokia_soft_geocoder_limit": false}', 'Hell');
@@ -22,4 +22,4 @@ SELECT cdb_geocoder_client._geocode_namedplace_point('evil_user', '{"is_organiza
 SELECT cdb_geocoder_client._geocode_namedplace_point('evil_user', '{"is_organization": false, "entity_name": "test_user"}', '{"street_geocoder_provider": "nokia","nokia_monthly_quota": 100, "nokia_soft_geocoder_limit": false}', 'Sheol', 'Hell', 'Ugly world');
 SELECT cdb_geocoder_client._geocode_postalcode_polygon('evil_user', '{"is_organization": false, "entity_name": "test_user"}', '{"street_geocoder_provider": "nokia","nokia_monthly_quota": 100, "nokia_soft_geocoder_limit": false}', '66666', 'Hell');
 SELECT cdb_geocoder_client._geocode_postalcode_point('evil_user', '{"is_organization": false, "entity_name": "test_user"}', '{"street_geocoder_provider": "nokia","nokia_monthly_quota": 100, "nokia_soft_geocoder_limit": false}', '66666', 'Hell');
-SELECT cdb_geocoder_client._geocode_ip_point('evil_user', '{"is_organization": false, "entity_name": "test_user"}', '{"street_geocoder_provider": "nokia","nokia_monthly_quota": 100, "nokia_soft_geocoder_limit": false}', '8.8.8.8');
+SELECT cdb_geocoder_client._geocode_ipaddress_point('evil_user', '{"is_organization": false, "entity_name": "test_user"}', '{"street_geocoder_provider": "nokia","nokia_monthly_quota": 100, "nokia_soft_geocoder_limit": false}', '8.8.8.8');
