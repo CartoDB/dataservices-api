f552f2db12aeb9324d3787866eb74e33b7ecade5
Mario de Frutos <mario.defrutos@cartodb.com>
2015-12-06 13:10:54 +0100

    Created helper to be used in all the tests

--- test/integration/test_admin0_functions.py
+++ test/integration/test_admin0_functions.py
@@ -1,29 +1,22 @@
-import os, time, requests, json
 from unittest import TestCase
 from nose.tools import assert_raises
+from nose.tools import assert_not_equal
+from ..helpers.integration_test_helper import IntegrationTestHelper
 
 
-class TestConfigHelper(TestCase):
+class TestAdmin0Functions(TestCase):
 
-  def setUp(self):
-    username = os.environ["GEOCODER_API_TEST_USERNAME"]
-    api_key = os.environ["GEOCODER_API_TEST_API_KEY"]
-    host = os.environ["GEOCODER_API_TEST_HOST"]
-    self.table_name = os.environ["GEOCODER_API_TEST_TABLE_NAME"]
-    self.sql_api_url = "https://{0}.{1}/api/v2/sql?api_key={2}".format(username, host, api_key)
+    def setUp(self):
+        self.env_variables = IntegrationTestHelper.get_environment_variables()
+        self.sql_api_url = "https://{0}.{1}/api/v2/sql?api_key={2}".format(
+            self.env_variables['username'],
+            self.env_variables['host'],
+            self.env_variables['api_key']
+        )
 
-  def test_if_select_with_admin0_is_ok(self):
-    query = "SELECT cdb_geocode_admin0_polygon(name) as geometry FROM {0} LIMIT 1".format(self.table_name)
-    geometry = self.execute_query(query)
-    assert geometry != None
-
-  def build_sql_api_query_url(self, query):
-    return "{0}&q={1}".format(self.sql_api_url,query)
-
-  def execute_query(self, query):
-    query_url = self.build_sql_api_query_url(query)
-    query_response = requests.get(query_url)
-    if query_response.status_code != 200:
-      raise Exception("Error executing SQL API query")
-    query_response_data = json.loads(query_response.text)
-    return query_response_data['rows'][0]['geometry']
+    def test_if_select_with_admin0_is_ok(self):
+        query = "SELECT cdb_geocode_admin0_polygon(name) as geometry " \
+            "FROM {0} LIMIT 1".format(
+            self.env_variables['table_name'])
+        geometry = IntegrationTestHelper.execute_query(self.sql_api_url, query)
+        assert_not_equal(geometry, None)
