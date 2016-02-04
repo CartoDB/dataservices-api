1367c46957cb3ecf9dba428bf23dd5d621a1e669
Mario de Frutos <mario.defrutos@cartodb.com>
2016-02-04 10:38:12 +0100

    Added automatic generated files warning header

--- client/sql/0.0.1/00_header.sql
+++ client/sql/0.0.1/00_header.sql
@@ -1,2 +1,3 @@
+--DO NOT MODIFY THIS FILE, IT IS GENERATED AUTOMATICALLY FROM SOURCES
 -- Complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION cdb_geocoder_client" to load this file. \quit
