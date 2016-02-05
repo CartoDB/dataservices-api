2a9d6c14bd75ac76b4f57f82772baa46c4366102
Mario de Frutos <mario.defrutos@cartodb.com>
2016-02-05 16:00:46 +0100

    Added metrics storage for internal functions and trim for all the params

---
+++ server/extension/cdb_geocoder_server--0.2.0--0.3.0.sql
@@ -0,0 +1,340 @@
+CREATE OR REPLACE FUNCTION cdb_geocoder_server.cdb_geocode_admin0_polygon(username text, orgname text, country_name text)
+RETURNS Geometry AS $$
+    from cartodb_services.metrics import QuotaService
+    from cartodb_services.metrics import InternalGeocoderConfig
+
+    plpy.execute("SELECT cdb_geocoder_server._connect_to_redis('{0}')".format(username))
+    redis_conn = GD["redis_connection_{0}".format(username)]['redis_metrics_connection']
+    user_geocoder_config = InternalGeocoderConfig(redis_conn, username, orgname)
+
+    quota_service = QuotaService(user_geocoder_config, redis_conn)
+    try:
+      plan = plpy.prepare("SELECT cdb_geocoder_server._cdb_geocode_admin0_polygon(trim($1)) AS mypolygon", ["text"])
+      rv = plpy.execute(plan, [plpy.quote_nullable(country_name)], 1)
+      result = rv[0]["mypolygon"]
+      if result:
+        quota_service.increment_success_geocoder_use()
+        return result
+      else:
+        quota_service.increment_empty_geocoder_use()
+        return None
+    except BaseException as e:
+      import sys, traceback
+      type_, value_, traceback_ = sys.exc_info()
+      quota_service.increment_failed_geocoder_use()
+      error_msg = 'There was an error trying to geocode using admin0 geocoder: {0}'.format(e)
+      plpy.notice(traceback.format_tb(traceback_))
+      plpy.error(error_msg)
+    finally:
+      quota_service.increment_total_geocoder_use()
+$$ LANGUAGE plpythonu;
+
+CREATE OR REPLACE FUNCTION cdb_geocoder_server.cdb_geocode_admin1_polygon(username text, orgname text, admin1_name text)
+RETURNS Geometry AS $$
+    from cartodb_services.metrics import QuotaService
+    from cartodb_services.metrics import InternalGeocoderConfig
+
+    plpy.execute("SELECT cdb_geocoder_server._connect_to_redis('{0}')".format(username))
+    redis_conn = GD["redis_connection_{0}".format(username)]['redis_metrics_connection']
+    user_geocoder_config = InternalGeocoderConfig(redis_conn, username, orgname)
+
+    quota_service = QuotaService(user_geocoder_config, redis_conn)
+    try:
+      plan = plpy.prepare("SELECT cdb_geocoder_server._cdb_geocode_admin1_polygon(trim($1)) AS mypolygon", ["text"])
+      rv = plpy.execute(plan, [plpy.quote_nullable(admin1_name)], 1)
+      result = rv[0]["mypolygon"]
+      if result:
+        quota_service.increment_success_geocoder_use()
+        return result
+      else:
+        quota_service.increment_empty_geocoder_use()
+        return None
+    except BaseException as e:
+      import sys, traceback
+      type_, value_, traceback_ = sys.exc_info()
+      quota_service.increment_failed_geocoder_use()
+      error_msg = 'There was an error trying to geocode using admin0 geocoder: {0}'.format(e)
+      plpy.notice(traceback.format_tb(traceback_))
+      plpy.error(error_msg)
+    finally:
+      quota_service.increment_total_geocoder_use()
+$$ LANGUAGE plpythonu;
+
+CREATE OR REPLACE FUNCTION cdb_geocoder_server.cdb_geocode_admin1_polygon(username text, orgname text, admin1_name text, country_name text)
+RETURNS Geometry AS $$
+    from cartodb_services.metrics import QuotaService
+    from cartodb_services.metrics import InternalGeocoderConfig
+
+    plpy.execute("SELECT cdb_geocoder_server._connect_to_redis('{0}')".format(username))
+    redis_conn = GD["redis_connection_{0}".format(username)]['redis_metrics_connection']
+    user_geocoder_config = InternalGeocoderConfig(redis_conn, username, orgname)
+
+    quota_service = QuotaService(user_geocoder_config, redis_conn)
+    try:
+      plan = plpy.prepare("SELECT cdb_geocoder_server._cdb_geocode_admin1_polygon(trim($1), trim($2)) AS mypolygon", ["text", "text"])
+      rv = plpy.execute(plan, [plpy.quote_nullable(admin1_name), plpy.quote_nullable(country_name)], 1)
+      result = rv[0]["mypolygon"]
+      if result:
+        quota_service.increment_success_geocoder_use()
+        return result
+      else:
+        quota_service.increment_empty_geocoder_use()
+        return None
+    except BaseException as e:
+      import sys, traceback
+      type_, value_, traceback_ = sys.exc_info()
+      quota_service.increment_failed_geocoder_use()
+      error_msg = 'There was an error trying to geocode using admin0 geocoder: {0}'.format(e)
+      plpy.notice(traceback.format_tb(traceback_))
+      plpy.error(error_msg)
+    finally:
+      quota_service.increment_total_geocoder_use()
+$$ LANGUAGE plpythonu;
+
+CREATE OR REPLACE FUNCTION cdb_geocoder_server.cdb_geocode_namedplace_point(username text, orgname text, city_name text)
+RETURNS Geometry AS $$
+    from cartodb_services.metrics import QuotaService
+    from cartodb_services.metrics import InternalGeocoderConfig
+
+    plpy.execute("SELECT cdb_geocoder_server._connect_to_redis('{0}')".format(username))
+    redis_conn = GD["redis_connection_{0}".format(username)]['redis_metrics_connection']
+    user_geocoder_config = InternalGeocoderConfig(redis_conn, username, orgname)
+
+    quota_service = QuotaService(user_geocoder_config, redis_conn)
+    try:
+      plan = plpy.prepare("SELECT cdb_geocoder_server._cdb_geocode_namedplace_point(trim($1)) AS mypoint", ["text"])
+      rv = plpy.execute(plan, [plpy.quote_nullable(city_name)], 1)
+      result = rv[0]["mypoint"]
+      if result:
+        quota_service.increment_success_geocoder_use()
+        return result
+      else:
+        quota_service.increment_empty_geocoder_use()
+        return None
+    except BaseException as e:
+      import sys, traceback
+      type_, value_, traceback_ = sys.exc_info()
+      quota_service.increment_failed_geocoder_use()
+      error_msg = 'There was an error trying to geocode using admin0 geocoder: {0}'.format(e)
+      plpy.notice(traceback.format_tb(traceback_))
+      plpy.error(error_msg)
+    finally:
+      quota_service.increment_total_geocoder_use()
+$$ LANGUAGE plpythonu;
+
+CREATE OR REPLACE FUNCTION cdb_geocoder_server.cdb_geocode_namedplace_point(username text, orgname text, city_name text, country_name text)
+RETURNS Geometry AS $$
+    from cartodb_services.metrics import QuotaService
+    from cartodb_services.metrics import InternalGeocoderConfig
+
+    plpy.execute("SELECT cdb_geocoder_server._connect_to_redis('{0}')".format(username))
+    redis_conn = GD["redis_connection_{0}".format(username)]['redis_metrics_connection']
+    user_geocoder_config = InternalGeocoderConfig(redis_conn, username, orgname)
+
+    quota_service = QuotaService(user_geocoder_config, redis_conn)
+    try:
+      plan = plpy.prepare("SELECT cdb_geocoder_server._cdb_geocode_namedplace_point(trim($1), trim($2)) AS mypoint", ["text", "text"])
+      rv = plpy.execute(plan, [plpy.quote_nullable(city_name), plpy.quote_nullable(country_name)], 1)
+      result = rv[0]["mypoint"]
+      if result:
+        quota_service.increment_success_geocoder_use()
+        return result
+      else:
+        quota_service.increment_empty_geocoder_use()
+        return None
+    except BaseException as e:
+      import sys, traceback
+      type_, value_, traceback_ = sys.exc_info()
+      quota_service.increment_failed_geocoder_use()
+      error_msg = 'There was an error trying to geocode using admin0 geocoder: {0}'.format(e)
+      plpy.notice(traceback.format_tb(traceback_))
+      plpy.error(error_msg)
+    finally:
+      quota_service.increment_total_geocoder_use()
+$$ LANGUAGE plpythonu;
+
+CREATE OR REPLACE FUNCTION cdb_geocoder_server.cdb_geocode_namedplace_point(username text, orgname text, city_name text, admin1_name text, country_name text)
+RETURNS Geometry AS $$
+    from cartodb_services.metrics import QuotaService
+    from cartodb_services.metrics import InternalGeocoderConfig
+
+    plpy.execute("SELECT cdb_geocoder_server._connect_to_redis('{0}')".format(username))
+    redis_conn = GD["redis_connection_{0}".format(username)]['redis_metrics_connection']
+    user_geocoder_config = InternalGeocoderConfig(redis_conn, username, orgname)
+
+    quota_service = QuotaService(user_geocoder_config, redis_conn)
+    try:
+      plan = plpy.prepare("SELECT cdb_geocoder_server._cdb_geocode_namedplace_point(trim($1), trim($2), trim($3)) AS mypoint", ["text", "text", "text"])
+      rv = plpy.execute(plan, [plpy.quote_nullable(city_name), plpy.quote_nullable(admin1_name), plpy.quote_nullable(country_name)], 1)
+      result = rv[0]["mypoint"]
+      if result:
+        quota_service.increment_success_geocoder_use()
+        return result
+      else:
+        quota_service.increment_empty_geocoder_use()
+        return None
+    except BaseException as e:
+      import sys, traceback
+      type_, value_, traceback_ = sys.exc_info()
+      quota_service.increment_failed_geocoder_use()
+      error_msg = 'There was an error trying to geocode using admin0 geocoder: {0}'.format(e)
+      plpy.notice(traceback.format_tb(traceback_))
+      plpy.error(error_msg)
+    finally:
+      quota_service.increment_total_geocoder_use()
+$$ LANGUAGE plpythonu;
+
+CREATE OR REPLACE FUNCTION cdb_geocoder_server.cdb_geocode_postalcode_point(username text, orgname text, code text)
+RETURNS Geometry AS $$
+    from cartodb_services.metrics import QuotaService
+    from cartodb_services.metrics import InternalGeocoderConfig
+
+    plpy.execute("SELECT cdb_geocoder_server._connect_to_redis('{0}')".format(username))
+    redis_conn = GD["redis_connection_{0}".format(username)]['redis_metrics_connection']
+    user_geocoder_config = InternalGeocoderConfig(redis_conn, username, orgname)
+
+    quota_service = QuotaService(user_geocoder_config, redis_conn)
+    try:
+      plan = plpy.prepare("SELECT cdb_geocoder_server._cdb_geocode_postalcode_point(trim($1)) AS mypoint", ["text"])
+      rv = plpy.execute(plan, [plpy.quote_nullable(code)], 1)
+      result = rv[0]["mypoint"]
+      if result:
+        quota_service.increment_success_geocoder_use()
+        return result
+      else:
+        quota_service.increment_empty_geocoder_use()
+        return None
+    except BaseException as e:
+      import sys, traceback
+      type_, value_, traceback_ = sys.exc_info()
+      quota_service.increment_failed_geocoder_use()
+      error_msg = 'There was an error trying to geocode using admin0 geocoder: {0}'.format(e)
+      plpy.notice(traceback.format_tb(traceback_))
+      plpy.error(error_msg)
+    finally:
+      quota_service.increment_total_geocoder_use()
+$$ LANGUAGE plpythonu;
+
+CREATE OR REPLACE FUNCTION cdb_geocoder_server.cdb_geocode_postalcode_point(username text, orgname text, code text, country text)
+RETURNS Geometry AS $$
+    from cartodb_services.metrics import QuotaService
+    from cartodb_services.metrics import InternalGeocoderConfig
+
+    plpy.execute("SELECT cdb_geocoder_server._connect_to_redis('{0}')".format(username))
+    redis_conn = GD["redis_connection_{0}".format(username)]['redis_metrics_connection']
+    user_geocoder_config = InternalGeocoderConfig(redis_conn, username, orgname)
+
+    quota_service = QuotaService(user_geocoder_config, redis_conn)
+    try:
+      plan = plpy.prepare("SELECT cdb_geocoder_server._cdb_geocode_postalcode_point(trim($1), trim($2)) AS mypoint", ["TEXT", "TEXT"])
+      rv = plpy.execute(plan, [plpy.quote_nullable(code), plpy.quote_nullable(country)], 1)
+      result = rv[0]["mypoint"]
+      if result:
+        quota_service.increment_success_geocoder_use()
+        return result
+      else:
+        quota_service.increment_empty_geocoder_use()
+        return None
+    except BaseException as e:
+      import sys, traceback
+      type_, value_, traceback_ = sys.exc_info()
+      quota_service.increment_failed_geocoder_use()
+      error_msg = 'There was an error trying to geocode using admin0 geocoder: {0}'.format(e)
+      plpy.notice(traceback.format_tb(traceback_))
+      plpy.error(error_msg)
+    finally:
+      quota_service.increment_total_geocoder_use()
+$$ LANGUAGE plpythonu;
+
+CREATE OR REPLACE FUNCTION cdb_geocoder_server.cdb_geocode_postalcode_polygon(username text, orgname text, code text)
+RETURNS Geometry AS $$
+    from cartodb_services.metrics import QuotaService
+    from cartodb_services.metrics import InternalGeocoderConfig
+
+    plpy.execute("SELECT cdb_geocoder_server._connect_to_redis('{0}')".format(username))
+    redis_conn = GD["redis_connection_{0}".format(username)]['redis_metrics_connection']
+    user_geocoder_config = InternalGeocoderConfig(redis_conn, username, orgname)
+
+    quota_service = QuotaService(user_geocoder_config, redis_conn)
+    try:
+      plan = plpy.prepare("SELECT cdb_geocoder_server._cdb_geocode_postalcode_polygon(trim($1)) AS mypolygon", ["text"])
+      rv = plpy.execute(plan, [plpy.quote_nullable(code)], 1)
+      result = rv[0]["mypolygon"]
+      if result:
+        quota_service.increment_success_geocoder_use()
+        return result
+      else:
+        quota_service.increment_empty_geocoder_use()
+        return None
+    except BaseException as e:
+      import sys, traceback
+      type_, value_, traceback_ = sys.exc_info()
+      quota_service.increment_failed_geocoder_use()
+      error_msg = 'There was an error trying to geocode using admin0 geocoder: {0}'.format(e)
+      plpy.notice(traceback.format_tb(traceback_))
+      plpy.error(error_msg)
+    finally:
+      quota_service.increment_total_geocoder_use()
+$$ LANGUAGE plpythonu;
+
+CREATE OR REPLACE FUNCTION cdb_geocoder_server.cdb_geocode_postalcode_polygon(username text, orgname text, code text, country text)
+RETURNS Geometry AS $$
+    from cartodb_services.metrics import QuotaService
+    from cartodb_services.metrics import InternalGeocoderConfig
+
+    plpy.execute("SELECT cdb_geocoder_server._connect_to_redis('{0}')".format(username))
+    redis_conn = GD["redis_connection_{0}".format(username)]['redis_metrics_connection']
+    user_geocoder_config = InternalGeocoderConfig(redis_conn, username, orgname)
+
+    quota_service = QuotaService(user_geocoder_config, redis_conn)
+    try:
+      plan = plpy.prepare("SELECT cdb_geocoder_server._cdb_geocode_postalcode_polygon(trim($1), trim($2)) AS mypolygon", ["TEXT", "TEXT"])
+      rv = plpy.execute(plan, [plpy.quote_nullable(code), plpy.quote_nullable(country)], 1)
+      result = rv[0]["mypolygon"]
+      if result:
+        quota_service.increment_success_geocoder_use()
+        return result
+      else:
+        quota_service.increment_empty_geocoder_use()
+        return None
+    except BaseException as e:
+      import sys, traceback
+      type_, value_, traceback_ = sys.exc_info()
+      quota_service.increment_failed_geocoder_use()
+      error_msg = 'There was an error trying to geocode using admin0 geocoder: {0}'.format(e)
+      plpy.notice(traceback.format_tb(traceback_))
+      plpy.error(error_msg)
+    finally:
+      quota_service.increment_total_geocoder_use()
+$$ LANGUAGE plpythonu;
+
+CREATE OR REPLACE FUNCTION cdb_geocoder_server.cdb_geocode_ipaddress_point(username text, orgname text, ip text)
+RETURNS Geometry AS $$
+    from cartodb_services.metrics import QuotaService
+    from cartodb_services.metrics import InternalGeocoderConfig
+
+    plpy.execute("SELECT cdb_geocoder_server._connect_to_redis('{0}')".format(username))
+    redis_conn = GD["redis_connection_{0}".format(username)]['redis_metrics_connection']
+    user_geocoder_config = InternalGeocoderConfig(redis_conn, username, orgname)
+
+    quota_service = QuotaService(user_geocoder_config, redis_conn)
+    try:
+      plan = plpy.prepare("SELECT cdb_geocoder_server._cdb_geocode_ipaddress_point(trim($1)) AS mypoint", ["TEXT"])
+      rv = plpy.execute(plan, [plpy.quote_nullable(ip)], 1)
+      result = rv[0]["mypoint"]
+      if result:
+        quota_service.increment_success_geocoder_use()
+        return result
+      else:
+        quota_service.increment_empty_geocoder_use()
+        return None
+    except BaseException as e:
+      import sys, traceback
+      type_, value_, traceback_ = sys.exc_info()
+      quota_service.increment_failed_geocoder_use()
+      error_msg = 'There was an error trying to geocode using admin0 geocoder: {0}'.format(e)
+      plpy.notice(traceback.format_tb(traceback_))
+      plpy.error(error_msg)
+    finally:
+      quota_service.increment_total_geocoder_use()
+$$ LANGUAGE plpythonu;
