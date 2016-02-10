f36a345db2de2b6cfe2a94b87e7fd1619e9df7be
Mario de Frutos <mario.defrutos@cartodb.com>
2016-02-10 22:26:53 +0100

    Updated python library version with routing part

--- server/lib/python/cartodb_services/setup.py
+++ server/lib/python/cartodb_services/setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 setup(
     name='cartodb_services',
 
-    version='0.1.0',
+    version='0.2.0',
 
     description='CartoDB Services API Python Library',
 
