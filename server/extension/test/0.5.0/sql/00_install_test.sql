2a807af6df17a92cb3c94596e0d2de1d2f07a1dc
Luis Bosque <luisico@gmail.com>
2016-02-22 10:06:09 +0100

    Fix upgrade/downgrade paths to 0.5.0

--- server/extension/test/0.5.0/sql/00_install_test.sql
+++ server/extension/test/0.5.0/sql/00_install_test.sql
@@ -9,8 +9,8 @@ CREATE EXTENSION cdb_geocoder;
 CREATE EXTENSION cdb_dataservices_server;
 
 -- Mock the redis server connection to point to this very test db
-SELECT cartodb.cdb_conf_setconf('redis_metrics_config', '{"sentinel_host": "localhost", "sentinel_port": 26379, "sentinel_master_id": "mymaster", "timeout": 0.1, "redis_db": 5}');
-SELECT cartodb.cdb_conf_setconf('redis_metadata_config', '{"sentinel_host": "localhost", "sentinel_port": 26379, "sentinel_master_id": "mymaster", "timeout": 0.1, "redis_db": 5}');
+SELECT cartodb.cdb_conf_setconf('redis_metrics_config', '{"redis_host": "localhost", "redis_port": 26379, "sentinel_master_id": "mymaster", "timeout": 0.1, "redis_db": 5}');
+SELECT cartodb.cdb_conf_setconf('redis_metadata_config', '{"redis_host": "localhost", "redis_port": 26379, "sentinel_master_id": "mymaster", "timeout": 0.1, "redis_db": 5}');
 
 -- Mock the varnish invalidation function
 -- (used by cdb_geocoder tests)
