c9fadc35663a538fc996195088062b5033f50326
Carla Iriberri <iriberrig@gmail.com>
2016-07-04 19:15:37 +0200

    everything

--- server/extension/cdb_dataservices_server--0.10.0--0.9.0.sql
+++
@@ -1,5 +0,0 @@
---DO NOT MODIFY THIS FILE, IT IS GENERATED AUTOMATICALLY FROM SOURCES
--- Complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "ALTER EXTENSION cdb_dataservices_server UPDATE TO '0.9.0'" to load this file. \quit
-DROP FUNCTION IF EXISTS cdb_dataservices_server._OBS_GetMeasureById(text, text, text, text, text, text);
-DROP FUNCTION IF EXISTS cdb_dataservices_server.OBS_GetMeasureById(text, text, text, text, text, text);
