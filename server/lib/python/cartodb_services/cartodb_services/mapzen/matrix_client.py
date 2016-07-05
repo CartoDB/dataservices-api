96199b0d6dfea835d6bb23bc87060e5732ef4094
Rafa de la Torre <rtorre@cartodb.com>
2016-07-05 16:18:59 +0200

    Add example to code doc

--- server/lib/python/cartodb_services/cartodb_services/mapzen/matrix_client.py
+++ server/lib/python/cartodb_services/cartodb_services/mapzen/matrix_client.py
@@ -3,6 +3,18 @@ import json
 
 class MatrixClient:
 
+    """
+    A minimal client for Mapzen Time-Distance Matrix Service
+
+    Example:
+
+    client = MatrixClient('your_api_key')
+    locations = [{"lat":40.744014,"lon":-73.990508},{"lat":40.739735,"lon":-73.979713},{"lat":40.752522,"lon":-73.985015},{"lat":40.750117,"lon":-73.983704},{"lat":40.750552,"lon":-73.993519}]
+    costing = 'pedestrian'
+
+    print client.one_to_many(locations, costing)
+    """
+
     ONE_TO_MANY_URL = 'https://matrix.mapzen.com/one_to_many'
 
     def __init__(self, matrix_key):
@@ -17,7 +29,7 @@ class MatrixClient:
 
     Returns:
         A dict with one_to_many, units and locations
-    """    
+    """
     def one_to_many(self, locations, costing):
         request_params = {
             'json': json.dumps({'locations': locations}),
