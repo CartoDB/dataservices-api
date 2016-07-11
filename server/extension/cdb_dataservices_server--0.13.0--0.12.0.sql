0eba673f5972b5a915a45ba56a2da6628ca60cf5
Carla <carla@cartodb.com>
2016-07-11 17:12:45 +0200

    drop functions before types

--- server/extension/cdb_dataservices_server--0.13.0--0.12.0.sql
+++ server/extension/cdb_dataservices_server--0.13.0--0.12.0.sql
@@ -3,11 +3,11 @@
 \echo Use "ALTER EXTENSION cdb_dataservices_server UPDATE TO '0.12.0'" to load this file. \quit
 
 -- HERE goes your code to upgrade/downgrade
-DROP TYPE IF EXISTS cdb_dataservices_server.ds_fdw_metadata;
-DROP TYPE IF EXISTS cdb_dataservices_server.ds_return_metadata;
-
 DROP FUNCTION IF EXISTS cdb_dataservices_server._OBS_ConnectUserTable(text, text, text, text, text, text);
 DROP FUNCTION IF EXISTS cdb_dataservices_server.__OBS_ConnectUserTable(text, text, text, text, text, text);
 DROP FUNCTION IF EXISTS cdb_dataservices_server._OBS_GetReturnMetadata(text, text, text, json);
 DROP FUNCTION IF EXISTS cdb_dataservices_server._OBS_FetchJoinFdwTableData(text, text, text, text, text, json);
 DROP FUNCTION IF EXISTS cdb_dataservices_server._OBS_DisconnectUserTable(text, text, text, text, text);
+
+DROP TYPE IF EXISTS cdb_dataservices_server.ds_fdw_metadata;
+DROP TYPE IF EXISTS cdb_dataservices_server.ds_return_metadata;
