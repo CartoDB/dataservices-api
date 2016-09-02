c39c1bcd49a1e66fad53fefc667cbc96cd3ea060
Mario de Frutos <mario.defrutos@cartodb.com>
2016-09-02 18:01:59 +0200

    Mapzen isochrones added

--- server/lib/python/cartodb_services/cartodb_services/mapzen/__init__.py
+++ server/lib/python/cartodb_services/cartodb_services/mapzen/__init__.py
@@ -2,3 +2,4 @@ from routing import MapzenRouting, MapzenRoutingResponse
 from isolines import MapzenIsolines
 from geocoder import MapzenGeocoder
 from matrix_client import MatrixClient
+from isochrones import MapzenIsochrones
