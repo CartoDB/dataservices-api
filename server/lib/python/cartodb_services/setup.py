b057001a599c93d66ab497c0ca5fa1a0c045a703
Carla Iriberri <iriberrig@gmail.com>
2016-09-12 14:17:27 +0200

    version test

--- server/lib/python/cartodb_services/setup.py
+++ server/lib/python/cartodb_services/setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 setup(
     name='cartodb_services',
 
-    version='0.8',
+    version='0.8.4',
 
     description='CartoDB Services API Python Library',
 
