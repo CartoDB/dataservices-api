c0451f48405e9781bf51280c942671be8f21becf
Carla Iriberri <iriberrig@gmail.com>
2016-09-14 10:43:34 +0200

    remove useless grant which makes everything explode

--- client/sql/95_grant_execute_manual.sql
+++ client/sql/95_grant_execute_manual.sql
@@ -1,4 +1,3 @@
 GRANT EXECUTE ON FUNCTION cdb_dataservices_client._DST_PrepareTableOBS_GetMeasure(output_table_name text, params json) TO publicuser;
 GRANT EXECUTE ON FUNCTION cdb_dataservices_client._DST_PopulateTableOBS_GetMeasure(table_name text, output_table_name text, params json) TO publicuser;
-GRANT EXECUTE ON FUNCTION cdb_dataservices_client._DST_PrepareTable_debug(output_table_name text, params json) TO publicuser;
 GRANT EXECUTE ON FUNCTION cdb_dataservices_client._DST_PopulateTable_debug(table_name text, output_table_name text, params json) TO publicuser;
