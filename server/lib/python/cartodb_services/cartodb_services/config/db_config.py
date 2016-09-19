5ae18003fa37e0d5d4dc1e9b566b6dcf9c014be7
Rafa de la Torre <rtorre@cartodb.com>
2016-09-19 13:17:59 +0200

    Use directly plpy in DBConfig class #273

--- server/lib/python/cartodb_services/cartodb_services/config/db_config.py
+++ server/lib/python/cartodb_services/cartodb_services/config/db_config.py
@@ -1,13 +1,12 @@
 from exceptions import *
+import plpy
 
 class DBConfig:
-    def __init__(self, plpy):
-        self._plpy = plpy
 
     def get(self, key):
         try:
             sql = "SELECT cartodb.CDB_Conf_GetConf('{0}') as conf".format(key)
-            conf = self._plpy.execute(sql, 1)
+            conf = plpy.execute(sql, 1)
             return conf[0]['conf']
         except Exception as e:
             raise ConfigException("Malformed config for {0}: {1}".format(key, e))
