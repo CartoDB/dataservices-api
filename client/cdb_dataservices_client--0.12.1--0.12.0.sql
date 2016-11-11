7f963d2f7a40c27b824f54bbd8f256f1f8ab7564
Mario de Frutos <mario.defrutos@cartodb.com>
2016-11-11 18:13:35 +0100

    Client update to type obs_meta_geometry with new types

--- client/cdb_dataservices_client--0.12.1--0.12.0.sql
+++ client/cdb_dataservices_client--0.12.1--0.12.0.sql
@@ -3,3 +3,10 @@
 \echo Use "ALTER EXTENSION cdb_dataservices_client UPDATE TO '0.12.0'" to load this file. \quit
 
 -- HERE goes your code to upgrade/downgrade
+ALTER TYPE cdb_dataservices_client.obs_meta_geometry DROP ATTRIBUTE score;
+ALTER TYPE cdb_dataservices_client.obs_meta_geometry DROP ATTRIBUTE numtiles;
+ALTER TYPE cdb_dataservices_client.obs_meta_geometry DROP ATTRIBUTE notnull_percent;
+ALTER TYPE cdb_dataservices_client.obs_meta_geometry DROP ATTRIBUTE numgeoms;
+ALTER TYPE cdb_dataservices_client.obs_meta_geometry DROP ATTRIBUTE percentfill;
+ALTER TYPE cdb_dataservices_client.obs_meta_geometry DROP ATTRIBUTE estnumgeoms;
+ALTER TYPE cdb_dataservices_client.obs_meta_geometry DROP ATTRIBUTE meanmediansize;
