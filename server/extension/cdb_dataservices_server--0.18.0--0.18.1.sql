8396eae733478d840a1f90e09772c4e90f0db607
Mario de Frutos <mario.defrutos@cartodb.com>
2016-11-11 18:13:01 +0100

    Server update to type obs_meta_geometry with new types

--- server/extension/cdb_dataservices_server--0.18.0--0.18.1.sql
+++ server/extension/cdb_dataservices_server--0.18.0--0.18.1.sql
@@ -3,3 +3,10 @@
 \echo Use "ALTER EXTENSION cdb_dataservices_server UPDATE TO '0.18.1'" to load this file. \quit
 
 -- HERE goes your code to upgrade/downgrade
+ALTER TYPE cdb_dataservices_server.obs_meta_geometry ADD ATTRIBUTE score numeric;
+ALTER TYPE cdb_dataservices_server.obs_meta_geometry ADD ATTRIBUTE numtiles bigint;
+ALTER TYPE cdb_dataservices_server.obs_meta_geometry ADD ATTRIBUTE notnull_percent numeric;
+ALTER TYPE cdb_dataservices_server.obs_meta_geometry ADD ATTRIBUTE numgeoms numeric;
+ALTER TYPE cdb_dataservices_server.obs_meta_geometry ADD ATTRIBUTE percentfill numeric;
+ALTER TYPE cdb_dataservices_server.obs_meta_geometry ADD ATTRIBUTE estnumgeoms numeric;
+ALTER TYPE cdb_dataservices_server.obs_meta_geometry ADD ATTRIBUTE meanmediansize numeric;
