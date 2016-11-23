4af5975ac14d49ee768f50c9fca4efb0eb1be0d7
Rafa de la Torre <rtorre@cartodb.com>
2016-11-23 17:07:23 +0100

    Add the internal geocoder to cdb_service_params #302

--- server/extension/sql/200_quotas.sql
+++ server/extension/sql/200_quotas.sql
@@ -85,6 +85,18 @@ RETURNS SETOF cdb_dataservices_server.service_params AS $$
   provider = user_obs_config.provider
   ret += [[service, monthly_quota, used_quota, soft_limit, provider]]
 
+  #-- Internal geocoder
+  service = 'internal_geocoder'
+  plpy.execute("SELECT cdb_dataservices_server._get_internal_geocoder_config({0}, {1})".format(plpy.quote_nullable(username), plpy.quote_nullable(orgname)))
+  user_geocoder_config = GD["user_internal_geocoder_config_{0}".format(username)]
+  user_service = UserMetricsService(user_geocoder_config, redis_conn)
+
+  monthly_quota = user_geocoder_config.monthly_quota
+  used_quota = user_service.used_quota(user_geocoder_config.service_type, today)
+  soft_limit = user_geocoder_config.soft_limit
+  provider = user_geocoder_config.provider
+  ret += [[service, monthly_quota, used_quota, soft_limit, provider]]
+
   return ret
 $$ LANGUAGE plpythonu;
 
