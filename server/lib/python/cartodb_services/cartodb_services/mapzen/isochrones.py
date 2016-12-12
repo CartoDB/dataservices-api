2953fda75cfd18cc29e85d1408b5588bbb0abe0c
Javier Goizueta <jgoizueta@gmail.com>
2016-12-12 12:14:10 +0100

    Revert num of isochrone retries to 3
    See #317

--- server/lib/python/cartodb_services/cartodb_services/mapzen/isochrones.py
+++ server/lib/python/cartodb_services/cartodb_services/mapzen/isochrones.py
@@ -13,7 +13,7 @@ class MapzenIsochrones:
     BASE_URL = 'https://matrix.mapzen.com/isochrone'
     READ_TIMEOUT = 60
     CONNECT_TIMEOUT = 10
-    MAX_RETRIES = 1
+    MAX_RETRIES = 3
 
     ACCEPTED_MODES = {
         "walk": "pedestrian",
