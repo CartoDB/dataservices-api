629555e1938b146668b8b435237af1b1a420001e
Javier Goizueta <jgoizueta@gmail.com>
2016-12-21 11:40:55 +0100

    Generate release 0.14.1
    This release cantains no actual code changes, only the
    use of search_path at the top of the install/migrate scripts

---
+++ client/cdb_dataservices_client--0.14.0--0.14.1.sql
@@ -0,0 +1,9 @@
+--DO NOT MODIFY THIS FILE, IT IS GENERATED AUTOMATICALLY FROM SOURCES
+-- Complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "ALTER EXTENSION cdb_dataservices_client UPDATE TO '0.14.1'" to load this file. \quit
+
+-- Make sure we have a sane search path to create/update the extension
+SET search_path = "$user",public,cdb_dataservices_client;
+
+-- This release introduces no changes other than the use of
+-- search path in the install and migration scripts
