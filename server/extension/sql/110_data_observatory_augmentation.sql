061956f250d9618fc22a1664bd8ddd8bac41329b
Mario de Frutos <mario.defrutos@cartodb.com>
2017-02-02 10:55:26 +0100

    Remove quota usage for obs_meta functions

--- server/extension/sql/110_data_observatory_augmentation.sql
+++ server/extension/sql/110_data_observatory_augmentation.sql
@@ -593,7 +593,6 @@ RETURNS TABLE (
   data JSON
 ) AS $$
   from cartodb_services.metrics import metrics
-  from cartodb_services.metrics import QuotaService
   from cartodb_services.tools import Logger,LoggerConfig
 
   plpy.execute("SELECT cdb_dataservices_server._connect_to_redis('{0}')".format(username))
@@ -604,27 +603,19 @@ RETURNS TABLE (
   plpy.execute("SELECT cdb_dataservices_server._get_logger_config()")
   logger_config = GD["logger_config"]
   logger = Logger(logger_config)
-  quota_service = QuotaService(user_obs_config, redis_conn)
-  if not quota_service.check_user_quota():
-    raise Exception('You have reached the limit of your quota')
 
   with metrics('obs_getdata', user_obs_config, logger):
     try:
         obs_plan = plpy.prepare("SELECT * FROM cdb_dataservices_server._OBS_GetData($1, $2, $3, $4, $5);", ["text", "text", "geomval[]", "json", "boolean"])
         result = plpy.execute(obs_plan, [username, orgname, geomvals, params, merge])
         if result:
-          quota_service.increment_success_service_use()
           return result
         else:
-          quota_service.increment_empty_service_use()
           return None
     except BaseException as e:
         import sys
-        quota_service.increment_failed_service_use()
         logger.error('Error trying to OBS_GetData', sys.exc_info(), data={"username": username, "orgname": orgname})
         raise Exception('Error trying to OBS_GetData')
-    finally:
-        quota_service.increment_total_service_use()
 $$ LANGUAGE plpythonu;
 
 CREATE OR REPLACE FUNCTION cdb_dataservices_server._OBS_GetData(
@@ -650,7 +641,6 @@ RETURNS TABLE (
   data JSON
 ) AS $$
   from cartodb_services.metrics import metrics
-  from cartodb_services.metrics import QuotaService
   from cartodb_services.tools import Logger,LoggerConfig
 
   plpy.execute("SELECT cdb_dataservices_server._connect_to_redis('{0}')".format(username))
@@ -661,29 +651,21 @@ RETURNS TABLE (
   plpy.execute("SELECT cdb_dataservices_server._get_logger_config()")
   logger_config = GD["logger_config"]
   logger = Logger(logger_config)
-  quota_service = QuotaService(user_obs_config, redis_conn)
-  if not quota_service.check_user_quota():
-    raise Exception('You have reached the limit of your quota')
 
   with metrics('obs_getdata', user_obs_config, logger):
     try:
         obs_plan = plpy.prepare("SELECT * FROM cdb_dataservices_server._OBS_GetData($1, $2, $3, $4);", ["text", "text", "text[]", "json"])
         result = plpy.execute(obs_plan, [username, orgname, geomrefs, params])
         if result:
-          quota_service.increment_success_service_use()
           return result
         else:
-          quota_service.increment_empty_service_use()
           return None
     except BaseException as e:
         import sys
-        quota_service.increment_failed_service_use()
         exc_info = sys.exc_info()
         logger.error('%s, %s, %s' % (exc_info[0], exc_info[1], exc_info[2]))
         logger.error('Error trying to OBS_GetData', exc_info, data={"username": username, "orgname": orgname})
         raise Exception('Error trying to OBS_GetData')
-    finally:
-        quota_service.increment_total_service_use()
 $$ LANGUAGE plpythonu;
 
 CREATE OR REPLACE FUNCTION cdb_dataservices_server._OBS_GetMeta(
@@ -709,7 +691,6 @@ CREATE OR REPLACE FUNCTION cdb_dataservices_server.OBS_GetMeta(
   target_geoms INTEGER DEFAULT NULL)
 RETURNS JSON AS $$
   from cartodb_services.metrics import metrics
-  from cartodb_services.metrics import QuotaService
   from cartodb_services.tools import Logger,LoggerConfig
 
   plpy.execute("SELECT cdb_dataservices_server._connect_to_redis('{0}')".format(username))
@@ -720,25 +701,17 @@ RETURNS JSON AS $$
   plpy.execute("SELECT cdb_dataservices_server._get_logger_config()")
   logger_config = GD["logger_config"]
   logger = Logger(logger_config)
-  quota_service = QuotaService(user_obs_config, redis_conn)
-  if not quota_service.check_user_quota():
-    raise Exception('You have reached the limit of your quota')
 
   with metrics('obs_getmeta', user_obs_config, logger):
     try:
         obs_plan = plpy.prepare("SELECT cdb_dataservices_server._OBS_GetMeta($1, $2, $3, $4, $5, $6, $7) as meta;", ["text", "text", "Geometry (Geometry, 4326)", "json", "integer", "integer", "integer"])
         result = plpy.execute(obs_plan, [username, orgname, geom, params, max_timespan_rank, max_score_rank, target_geoms])
         if result:
-          quota_service.increment_success_service_use()
           return result[0]['meta']
         else:
-          quota_service.increment_empty_service_use()
           return None
     except BaseException as e:
         import sys
-        quota_service.increment_failed_service_use()
         logger.error('Error trying to OBS_GetMeta', sys.exc_info(), data={"username": username, "orgname": orgname})
         raise Exception('Error trying to OBS_GetMeta')
-    finally:
-        quota_service.increment_total_service_use()
 $$ LANGUAGE plpythonu;
