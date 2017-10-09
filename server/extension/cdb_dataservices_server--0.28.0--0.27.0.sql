e68cc3e820840118d666d9b906cc3c9acbd5532d
Mario de Frutos <mario.defrutos@cartodb.com>
2017-10-09 17:20:57 +0200

    OBS_GetAvailableGeometries now has number of geometries parameter
    We've add a new parameter, number of geometries, that would be used
    by DO to compute the geometry that better fit with the input provided

--- server/extension/cdb_dataservices_server--0.28.0--0.27.0.sql
+++ server/extension/cdb_dataservices_server--0.28.0--0.27.0.sql
@@ -3,3 +3,15 @@
 \echo Use "ALTER EXTENSION cdb_dataservices_server UPDATE TO '0.27.0'" to load this file. \quit
 
 -- HERE goes your code to upgrade/downgrade
+CREATE OR REPLACE FUNCTION cdb_dataservices_server.OBS_GetAvailableGeometries(
+  username TEXT,
+  orgname TEXT,
+  bounds geometry(Geometry, 4326) DEFAULT NULL,
+  filter_tags TEXT[] DEFAULT NULL,
+  numer_id TEXT DEFAULT NULL,
+  denom_id TEXT DEFAULT NULL,
+  timespan TEXT DEFAULT NULL)
+RETURNS SETOF cdb_dataservices_server.obs_meta_geometry AS $$
+  CONNECT cdb_dataservices_server._obs_server_conn_str(username, orgname);
+  SELECT * FROM cdb_observatory.OBS_GetAvailableGeometries(bounds, filter_tags, numer_id, denom_id, timespan);
+$$ LANGUAGE plproxy;
