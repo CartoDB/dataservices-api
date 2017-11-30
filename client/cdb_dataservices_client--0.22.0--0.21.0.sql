d2980aa30cb681f6f8a127cd0afc3a2d09d232d7
Rafa de la Torre <rtorre@carto.com>
2017-11-30 12:41:39 +0100

    Remove duplicate header from downgrade script

--- client/cdb_dataservices_client--0.22.0--0.21.0.sql
+++ client/cdb_dataservices_client--0.22.0--0.21.0.sql
@@ -6,12 +6,6 @@
 SET search_path = "$user",cartodb,public,cdb_dataservices_client;
 
 -- HERE goes your code to upgrade/downgrade
---DO NOT MODIFY THIS FILE, IT IS GENERATED AUTOMATICALLY FROM SOURCES
--- Complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "CREATE EXTENSION cdb_dataservices_client" to load this file. \quit
-
--- Make sure we have a sane search path to create/update the extension
-SET search_path = "$user",cartodb,public,cdb_dataservices_client;
 --
 -- Geocoder server connection config
 --
