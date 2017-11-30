048aebedd9b3f719deeb88283d4f0d2e91970454
Rafa de la Torre <rtorre@carto.com>
2017-11-30 13:08:54 +0100

    Remove permissions stuff from downgrade script

--- server/extension/cdb_dataservices_server--0.29.0--0.28.0.sql
+++ server/extension/cdb_dataservices_server--0.29.0--0.28.0.sql
@@ -3017,18 +3017,3 @@ RETURNS SETOF cdb_dataservices_server.isoline AS $$
   result = plpy.execute(mapzen_plan, [username, orgname, source, mode, range, options])
   return result
 $$ LANGUAGE plpythonu;
-DO $$
-BEGIN
-    IF NOT EXISTS (
-        SELECT *
-        FROM   pg_catalog.pg_user
-        WHERE  usename = 'geocoder_api') THEN
-
-            CREATE USER geocoder_api;
-    END IF;
-    GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA cdb_dataservices_server TO geocoder_api;
-    GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO geocoder_api;
-    GRANT USAGE ON SCHEMA cdb_dataservices_server TO geocoder_api;
-    GRANT USAGE ON SCHEMA public TO geocoder_api;
-    GRANT SELECT ON ALL TABLES IN SCHEMA public TO geocoder_api;
-END$$;
