23e3de9da5d244d92882da8c085605ec1bbe70ba
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2018-07-05 15:10:05 +0200

    Add missing permission for cdb_bulk_geocode_street_point

--- client/sql/95_grant_execute_manual.sql
+++ client/sql/95_grant_execute_manual.sql
@@ -1,3 +1,4 @@
 GRANT EXECUTE ON FUNCTION cdb_dataservices_client._DST_PrepareTableOBS_GetMeasure(output_table_name text, params json) TO publicuser;
 GRANT EXECUTE ON FUNCTION cdb_dataservices_client._DST_PopulateTableOBS_GetMeasure(table_name text, output_table_name text, params json) TO publicuser;
 GRANT EXECUTE ON FUNCTION cdb_dataservices_client._OBS_PreCheck(source_query text, params JSON) TO publicuser;
+GRANT EXECUTE ON FUNCTION cdb_dataservices_client.cdb_bulk_geocode_street_point(query text, street_column text, city_column text, state_column text, country_column text, batch_size integer) TO publicuser;
