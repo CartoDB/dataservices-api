089d3e3f276bc5c102c884cc486cd5feaf9ece78
Mario de Frutos <ethervoid@gmail.com>
2018-11-05 16:51:17 +0100

    New version 0.34.1

---
+++ server/extension/cdb_dataservices_server--0.34.1--0.34.0.sql
@@ -0,0 +1,5 @@
+--DO NOT MODIFY THIS FILE, IT IS GENERATED AUTOMATICALLY FROM SOURCES
+-- Complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "ALTER EXTENSION cdb_dataservices_server UPDATE TO '0.34.0'" to load this file. \quit
+
+-- HERE goes your code to upgrade/downgrade
