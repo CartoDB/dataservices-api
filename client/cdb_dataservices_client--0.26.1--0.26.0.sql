0dfd1a9caa417c4da1601680032302d6d312af75
Javier Torres <jtorres@carto.com>
2018-12-24 17:28:56 +0100

    Update/downgrade scripts

---
+++ client/cdb_dataservices_client--0.26.1--0.26.0.sql
@@ -0,0 +1,9 @@
+--DO NOT MODIFY THIS FILE, IT IS GENERATED AUTOMATICALLY FROM SOURCES
+-- Complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "ALTER EXTENSION cdb_dataservices_client UPDATE TO '0.26.0'" to load this file. \quit
+
+-- Make sure we have a sane search path to create/update the extension
+SET search_path = "$user",cartodb,public,cdb_dataservices_client;
+
+-- HERE goes your code to upgrade/downgrade
+ALTER TYPE cdb_dataservices_client.geocoding ALTER ATTRIBUTE the_geom SET TYPE geometry(MultiPolygon,4326);
---
+++ server/extension/cdb_dataservices_server--0.35.0--0.35.1.sql
@@ -0,0 +1,6 @@
+--DO NOT MODIFY THIS FILE, IT IS GENERATED AUTOMATICALLY FROM SOURCES
+-- Complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "ALTER EXTENSION cdb_dataservices_server UPDATE TO '0.35.1'" to load this file. \quit
+
+-- HERE goes your code to upgrade/downgrade
+ALTER TYPE cdb_dataservices_server.geocoding ALTER ATTRIBUTE the_geom SET TYPE geometry(Point,4326);
