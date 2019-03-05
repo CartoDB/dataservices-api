6a57a85e724d65fd915b8d5c8467c11201d48fe0
Javier Goizueta <jgoizueta@gmail.com>
2019-03-05 11:55:40 +0100

    Release new python lib version 0.21.3

--- server/lib/python/cartodb_services/setup.py
+++ server/lib/python/cartodb_services/setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 setup(
     name='cartodb_services',
 
-    version='0.21.2',
+    version='0.21.3',
 
     description='CartoDB Services API Python Library',
 
