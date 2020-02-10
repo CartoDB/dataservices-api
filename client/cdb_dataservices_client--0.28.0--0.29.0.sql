02d705465c4561fb52feebdf0e162bb4f35fceb8
antoniocarlon <antoniocarlon@gmail.com>
2020-02-10 15:45:51 +0100

    Removed unneeded message

--- client/cdb_dataservices_client--0.28.0--0.29.0.sql
+++ client/cdb_dataservices_client--0.28.0--0.29.0.sql
@@ -1,4 +1,3 @@
---DO NOT MODIFY THIS FILE, IT IS GENERATED AUTOMATICALLY FROM SOURCES
 -- Complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "ALTER EXTENSION cdb_dataservices_client UPDATE TO '0.29.0'" to load this file. \quit
 
