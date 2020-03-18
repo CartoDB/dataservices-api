09e24e13d3c4a42d2fec81b197634b669457ea66
antoniocarlon <antoniocarlon@gmail.com>
2020-03-18 12:09:12 +0100

    Fixing is_destination for HERE isolines

--- server/lib/python/cartodb_services/test/test_heremapsrouting.py
+++ server/lib/python/cartodb_services/test/test_heremapsrouting.py
@@ -133,6 +133,7 @@ class HereMapsRoutingIsolineTestCase(unittest.TestCase):
         self.isoline_url = "{0}{1}".format(HereMapsRoutingIsoline.PRODUCTION_ROUTING_BASE_URL,
                                      HereMapsRoutingIsoline.ISOLINE_PATH)
 
+
     def test_calculate_isodistance_with_valid_params(self, req_mock):
         url = "{0}?start=geo%2133.0%2C1.0&mode=shortest%3Bcar".format(self.isoline_url)
         req_mock.register_uri('GET', url, text=self.GOOD_RESPONSE)
@@ -208,9 +209,21 @@ class HereMapsRoutingIsolineTestCase(unittest.TestCase):
                               text=self.GOOD_RESPONSE)
         response = self.routing.calculate_isochrone('geo!33.0,1.0', 'car',
                                                     ['1000', '2000'],
+                                                    ['is_destination=false'])
+        parsed_url = urlparse(req_mock.request_history[0].url)
+        url_params = parse_qs(parsed_url.query)
+
+        self.assertEqual(url_params['start'][0], 'geo!33.0,1.0')
+
+    def test_destination_parameters_works_properly(self, req_mock):
+        req_mock.register_uri('GET', requests_mock.ANY,
+                              text=self.GOOD_RESPONSE)
+        response = self.routing.calculate_isochrone('geo!33.0,1.0', 'car',
+                                                    ['1000', '2000'],
                                                     ['is_destination=true'])
         parsed_url = urlparse(req_mock.request_history[0].url)
         url_params = parse_qs(parsed_url.query)
+
         self.assertEqual(url_params['destination'][0], 'geo!33.0,1.0')
 
     def test_isodistance_with_nonstandard_url(self, req_mock):
