f59919efefb78fffff564ec17c55f6df644e8d7e
Mmoncadaisla <mmoncadaisla@gmail.com>
2021-05-13 09:40:53 +0200

    Add new imports for apikey parameter support

--- server/lib/python/cartodb_services/cartodb_services/here/__init__.py
+++ server/lib/python/cartodb_services/cartodb_services/here/__init__.py
@@ -1,3 +1,4 @@
-from cartodb_services.here.geocoder import HereMapsGeocoder
-from cartodb_services.here.bulk_geocoder import HereMapsBulkGeocoder
+from cartodb_services.here.geocoder import HereMapsGeocoder, HereMapsGeocoderV7
+from cartodb_services.here.bulk_geocoder import HereMapsBulkGeocoder, HereMapsBulkGeocoderV7
+from cartodb_services.here.service_factory import get_geocoder, get_bulk_geocoder, get_routing_isoline
 from cartodb_services.here.routing import HereMapsRoutingIsoline
