86c10f0ca521514f7a079aa4fdf636c314715704
Moisés Calzado <mcalzado@cartodb.com>
2022-09-02 11:42:16 +0200

    Bump version

--- server/lib/python/cartodb_services/setup.py
+++ server/lib/python/cartodb_services/setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 setup(
     name='cartodb_services',
 
-    version='0.24.1',
+    version='0.24.2.dev',
 
     description='CartoDB Services API Python Library',
 
